6f505b314b04591e9a3d2e1f79eac1b0985180b0
User@Desktop-3D4BBRS <kebrew2@gmail.com>
2019-02-20 14:01:58 -0500

    Initial commit

---
+++ app/src/main/java/foodshare/kyle740/github/io/foodshare/LogIn.java
@@ -0,0 +1,13 @@
+package foodshare.kyle740.github.io.foodshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
